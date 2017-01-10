b62a461955133a54d3fd43ee20023daa48853387
Johannes Manz <johannes_manz@gmx.de>
2017-01-10 21:53:48 +0100

    Start of new game: BananaOrbit

---
+++ NanoGames.Games/BananaOrbit/BananaOrbitMatch.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) the authors of nanoGames. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt in the project root.
+
+using System;
+
+namespace NanoGames.Games.BananaOrbit
+{
+    class BananaOrbitMatch : Match<BananaOrbitPlayer>
+    {
+        protected override void Initialize()
+        {
+
+        }
+
+        protected override void Update()
+        {
+            DrawScreen();
+        }
+
+        private void DrawScreen()
+        {
+            Output.Graphics.Circle(new Color(1, 1, 1), new Vector(160, 100), 20);
+        }
+
+        private void PlayAudio()
+        {
+
+        }
+    }
+}
---
+++ NanoGames.Games/BananaOrbit/BananaOrbitPlayer.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) the authors of nanoGames. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt in the project root.
+
+using System;
+
+namespace NanoGames.Games.BananaOrbit
+{
+    class BananaOrbitPlayer : Player<BananaOrbitMatch>
+    {
+    }
+}
