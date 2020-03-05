35b7e44f560f4a657271187ab90dfbcbf6510201
Laptop Johannes Manz <johannes_manz@gmx.de>
2020-03-05 18:24:27 +0100

    midlines are calulated in voronoigraph. Nothin is tested so far

--- NanoGames.Games/Cluster/VoronoiGraph.cs
+++ NanoGames.Games/Cluster/VoronoiGraph.cs
@@ -3,13 +3,16 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq
+using System.
 
 namespace NanoGames.Games.Cluster
 {
     internal class VoronoiGraph
     {
         public int LengthPoints;
-        public List<Vector> CenterPoints;
+        //public List<Vector> CenterPoints;
+        public Vector[] CenterPoints = { };
         public List<Tile> Tiles;
         public double RadBoundary;
 
@@ -31,7 +34,7 @@ namespace NanoGames.Games.Cluster
             return Tiles;
         }
 
-        private Tile getOneTile(int idx_thisPoint)
+        private Tile getOneTile(int idx_thisCenterPoint)
         {
             /*int idx_nearestNeighbor = getIndexNearestNeighbor(idx_thisPoint);
 
@@ -53,30 +56,56 @@ namespace NanoGames.Games.Cluster
             newTile.Segments.Add(new Segment())*/
 
             // procedere:
-            // a1) sort all points by their distance
-            // a) find all midlines. Midlines have the form m = p + k * d. k is a scalar.
-            // b) find all intersections od midlines. in this list are all the points of the graph
-            // c) go through list of intersectoins and find the ones. distance to this_point <= distance all other points.
-
-            List<Vector> midlines_p = new List<Vector> { };
-            List<Vector> midlines_d = new List<Vector> { };
+            // a) 
+            // b) find all midlines. Midlines have the form m = p + k * d. k is a scalar.
+            
+            // d) go through list of intersectoins and find the ones. distance to this_point <= distance all other points.
 
 
+            var thisCenterPoint = CenterPoints[idx_thisCenterPoint];
 
+            // sort all centerPoints by their distance to thisCenterPoint
+            var distancesSquared = CenterPoints.Select(x => (x - thisCenterPoint).Length).ToList();
+            var sortedCenterPoints = CenterPoints.Zip(distancesSquared, (x, y) => (x: x, y: y)).OrderBy(t => t.y).Select(t => t.x).ToList();
 
-        }
+            // find all midLines. Midlines have the form midline = m + k * d. k is a scalar.
+            var m = sortedCenterPoints.Select(x => 0.5 * (x - thisCenterPoint)).ToList();
+            var d = m.Select(x => (x - thisCenterPoint).RotatedLeft).ToList();
+            var midlines = m.Zip(d, (a, b) => (m: a, d: b)).ToList();
 
-        private List<Vector> sortByDistance(int idx_thisCenterPoint)
-        {
-            Vector this_centerPoint = CenterPoints[idx_thisCenterPoint];
-            List<double> distancesSquared = new List<double> { };
-
-            for (int i = 0; i < LengthPoints)
+            // find all intersections od midlines. In this list are all the points of the final voronoi graph
+            var counter = 1;
+            var length = CenterPoints.Length;
+            List<Vector> intersections = new List<Vector { };
+            foreach (var t in midlines)
             {
-                distancesSquared.Add()
+                for (int idx = counter; idx < length; idx++)
+                {
+                    Vector m1 = t.m;
+                    Vector m2 = midlines[idx].m;
+                    Vector d1 = t.d;
+                    Vector d2 = midlines[idx].d;
+
+                    double denom = d1.Y * d2.X - d1.X * d2.Y;
+                    // check if denom is zero
+                    double k1 = (d2.Y * m1.X - d2.X * m1.Y - d2.Y * m2.X + d2.X * m2.Y) / denom;
+                    // from wolfram alpha k1 = (d22 m11 - d21 m12 - d22 m21 + d21 m22) / (d12 d21 - d11 d22);
+                    Vector intersection = m1 + k1 * d1;
+                    intersections.Add(intersection);
+                }
+
+                counter++;
             }
 
+
+
+
+
+
+
         }
+
+
 
         private int getIndexNearestNeighbor(int idx_thisPoint)
         {
